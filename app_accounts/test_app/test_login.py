da25871719b2bab79566e5f0b83c8208a01020c5
Marc Schaar <m-schaar@gmx.net>
2025-11-01 17:29:03 +0100

    fix: login test respose data testing

--- app_accounts/test_app/test_login.py
+++ app_accounts/test_app/test_login.py
@@ -7,9 +7,9 @@ from app_accounts.models import User
 
 class TestLogin(APITestCase):
     def setUp(self):
-        self.user = User.objects.create_user(username="exampleUsername", password="examplePassword")
+        self.user = User.objects.create_user(username="exampleUsername", password="examplePassword", email="email@mail.de")
 
-    def test_login_user(self):
+    def test_login_user_200(self):
         url = reverse("login-list")
         user_client = APIClient()
         data = {
@@ -19,6 +19,14 @@ class TestLogin(APITestCase):
 
         response = user_client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("token", response.data)
+        self.assertEqual(response.data["username"], self.user.username)
+        self.assertEqual(response.data["user_id"], 1)
+        self.assertEqual(response.data["email"], self.user.email)
+
+    def test_login_user_400(self):
+        url = reverse("login-list")
+        user_client = APIClient()
 
         data = {
         "username": "",
@@ -27,7 +35,10 @@ class TestLogin(APITestCase):
 
         response = user_client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
+    
+    def test_login_user_400(self):
+        url = reverse("login-list")
+        user_client = APIClient()
         data = {
         "username": "exampleUsername",
         "password": ""
