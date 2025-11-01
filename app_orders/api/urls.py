f3d210c4eef40f0e098d36f0446bb842012d3476
Marc Schaar <m-schaar@gmx.net>
2025-11-01 19:11:23 +0100

    fix:urls  missing / in detail urls

--- app_orders/api/urls.py
+++ app_orders/api/urls.py
@@ -3,7 +3,7 @@ from .views import OrdersListView, OrderDetailView, OrderCountView
 
 urlpatterns = [
     path("orders/", OrdersListView.as_view(), name="order-list"),
-    path("orders/<int:pk>", OrderDetailView.as_view(), name="order-detail"),
+    path("orders/<int:pk>/", OrderDetailView.as_view(), name="order-detail"),
     path(
         "order-count/<int:pk>/",
         OrderCountView.as_view(completed=False),
