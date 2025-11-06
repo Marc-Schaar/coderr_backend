f5d5e3f9adefd1eb24002217cac310b9dce85bdb
Marc S. <153436997+Marc-Schaar@users.noreply.github.com>
2025-11-06 16:11:56 +0100

    Add static file settings to Django project

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
