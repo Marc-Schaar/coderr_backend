b17eb219eaf114118c7789863eade2b718e9059c
m-schaar@gmx.net <m-schaar@gmx.net>
2026-07-18 13:30:05 +0200

    Füge Umgebungsvariablen für PostgreSQL und Gunicorn-Bindung hinzu

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 python manage.py migrate --noinput
 
 exec gunicorn core.wsgi:application \
-    --bind 0.0.0.0:8000 \
+    --bind "${GUNICORN_BIND:-0.0.0.0:8000}" \
     --workers "${GUNICORN_WORKERS:-3}" \
     --access-logfile - \
     --error-logfile -
